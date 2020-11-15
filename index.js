50a32bf9d628314f3ec5de33a9ce4fab57bf3464
BHessel <hesselfitness@gmail.com>
2020-11-15 08:30:48 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+//1
+document.querySelector('main').remove();
+//2 + 3
+let newHeader = document.createElement('h1')
+document.body.appendChild(newHeader).id = "victory"
+//4
+newHeader.innerHTML = "Ben is the champion"
